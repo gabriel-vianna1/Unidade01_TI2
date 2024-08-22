7ed4fc357d67a86ab5a22b9058c5f215c5c85718
Gabriel Costa Vianna <gabrielvianna495@gmail.com>
2024-08-21 21:32:11 -0300

    primeiro commit.

---
+++ Unidade01/src/Unidade01/SomarDoisNumero.java
@@ -0,0 +1,18 @@
+package Unidade01;
+import java.util.*;
+class SomarDoisNumero {
+ public static Scanner sc = new Scanner(System.in);
+ public static void main(String[] args) {
+	 int num1, num2, soma;
+	 
+	 System.out.println("Digite um número");
+     num1 = sc.nextInt();
+     System.out.println("Digite outro némero");
+     num2 = sc.nextInt();
+     
+     soma = (num1 + num2);
+     
+     System.out.println("Soma: " + soma);
+ }
+ 
+}
